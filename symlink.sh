6052e03b99b1d8e027f6ce451dc804257f5fe74c
boss <boss@bosss-MacBook-Pro.local>
2014-01-16 08:57:24 -0800

    modified dotfile dirpath

--- symlink.sh
+++ symlink.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
 DIRS=`ls -F | grep /`
+PWD=`pwd`
 
 for DIR in $DIRS
 do
-    DOTFILES=`ls $HOME/dotfiles/$DIR`
+    DOTFILES=`ls $DIR`
     for DOTFILE in $DOTFILES
     do
         if [ ! -e $HOME/.$DOTFILE ]; then
-            ln -s $HOME/dotfiles/$DIR/$DOTFILE $HOME/.$DOTFILE
+            ln -s $PWD/$DIR$DOTFILE $HOME/.$DOTFILE
             echo "$HOME/.$DOTFILE created"
         else
             echo "$HOME/.$DOTFILE already exists"
